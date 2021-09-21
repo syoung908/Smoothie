a289af5179935baf3c379bb0f295ff39385476cf
syoung908 <syoung908@gmail.com>
2021-09-21 10:04:38 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace SmoothieProgram {
+    public class Program {
+        public static void Main() {
+            Smoothie s1 = new(new string[] { "Banana" });
+            foreach (string ingredient in s1.Ingredients) {
+                Console.WriteLine(ingredient);
+            }
+
+            Console.WriteLine(s1.GetCost());
+            Console.WriteLine(s1.GetPrice());
+            Console.WriteLine(s1.GetName());
+
+            Smoothie s2 = new(new string[] { "Raspberries", "Strawberries", "Blueberries" });
+            foreach (string ingredient in s2.Ingredients) {
+                Console.WriteLine(ingredient);
+            }
+
+            Console.WriteLine(s2.GetCost());
+            Console.WriteLine(s2.GetPrice());
+            Console.WriteLine(s2.GetName());
+        }
+    }
+}
---
+++ Smoothie.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmoothieProgram {
+    public class Smoothie {
+        public string[] Ingredients { get; private set; }
+
+        private readonly Dictionary<string, double> _prices = new() {
+            { "Strawberries", 1.50 },
+            { "Banana", 0.50 },
+            { "Mango", 2.50 },
+            { "Blueberries", 1.00 },
+            { "Raspberries", 1.00 },
+            { "Apple", 1.75 },
+            { "Pineapple", 3.50 }
+        };
+
+        public Smoothie(string[] ingredients) {
+            Ingredients = ingredients;
+
+            foreach (string ingredient in ingredients) {
+                if (!_prices.ContainsKey(ingredient)) {
+                    throw new ArgumentException("Invalid ingredient.");
+                }
+            }
+        }
+
+        private double CalculateCost() {
+            double total = 0.0;
+            foreach (string ingredient in Ingredients) {
+                total += _prices[ingredient];
+            }
+            return total;
+        }
+
+        public string GetCost() {
+            return string.Format("${0:f2}", CalculateCost());
+        }
+
+        public string GetPrice() {
+            double cost = CalculateCost();
+            double price = Math.Round(cost + (cost * 1.5), 2);
+            return string.Format("${0:f2}", price);
+        }
+
+        public string GetName() {
+            StringBuilder builder = new();
+            string[] sortedIngredients = Ingredients;
+            Array.Sort(sortedIngredients);
+
+            foreach (string ingredient in sortedIngredients) {
+                if(builder.Length != 0) {
+                    builder.Append(' ');
+                }
+                builder.Append(ingredient.Replace("berries", "berry"));
+            }
+
+            if (Ingredients.Length > 1) {
+                builder.Append(" Fusion");
+            } else {
+                builder.Append(" Smoothie");
+            }
+            return builder.ToString();
+        }
+
+    }
+}
